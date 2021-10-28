1860c0f4fe8a455fa4e3af9b2d813ed6565838c4
MaxKliuba <klyubamaks@gmail.com>
2021-10-28 13:34:35 +0300

    README.md upd

---
+++ Lab1/Lab1/Task0/Task0.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace Lab1
+{
+    class Task0
+    {
+        public static void Run(string input)
+        {
+            string inputBinDecoded = FromBinString(input);
+            //Console.WriteLine(inputBinDecoded);
+
+            string inputBase64Decoded = Encoding.UTF8.GetString(Convert.FromBase64String(inputBinDecoded));
+            Console.WriteLine(inputBase64Decoded);
+        }
+
+        private static string FromBinString(string binString)
+        {
+            var bytes = new byte[binString.Length / 8];
+
+            for (var i = 0; i < bytes.Length; i++)
+            {
+                bytes[i] = Convert.ToByte(binString.Substring(i * 8, 8), 2);
+            }
+
+            return Encoding.UTF8.GetString(bytes);
+        }
+    }
+}
---
+++ Lab3/README.md
@@ -0,0 +1,2 @@
+# TASK
+https://docs.google.com/document/d/1HY7Dl-5itYD3C_gkueBvvBFpT4CecGPiR30BsARlTpQ/edit
